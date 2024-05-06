da8dcfa99d44b5ed3c6569a31bc7d0442a4e5551
Panuthep <falcon_270394@hotmail.co.th>
2024-05-06 23:18:26 +0700

    uc

--- scripts/pretokenize_dataset.py
+++ scripts/pretokenize_dataset.py
@@ -17,6 +17,7 @@ def read_dataset(
         total: int | None = None,
         max_samples: int | None = None
 ) -> List[Doc]:
+    spans_num = 0
     with open(path, "r") as f:
         docs = []
         for line in tqdm(f, desc="Reading dataset", total=total, unit=" samples"):
@@ -35,11 +36,12 @@ def read_dataset(
                 continue
             
             doc.spans = valid_spans
+            spans_num += len(valid_spans)
 
             docs.append(doc)
             if max_samples and len(docs) >= max_samples:
                 break
-    return docs
+    return docs, spans_num
 
 
 if __name__ == "__main__":
@@ -54,22 +56,23 @@ if __name__ == "__main__":
     parser.add_argument("--train_end_index", type=int, default=None)
     args = parser.parse_args()
 
-    docs: List[Doc] = read_dataset(args.dataset_path, total=args.dataset_size, max_samples=args.max_samples)
+    docs, spans_num = read_dataset(args.dataset_path, total=args.dataset_size, max_samples=args.max_samples)
     print(f"Dataset size: {len(docs)}")
+    print(f"Spans num: {spans_num}")
 
-    if args.train_end_index is None:
-        args.train_end_index = len(docs)
+    # if args.train_end_index is None:
+    #     args.train_end_index = len(docs)
 
-    train_docs = docs[args.train_start_index:args.train_end_index]
-    # val_docs = docs[:100]
+    # train_docs = docs[args.train_start_index:args.train_end_index]
+    # # val_docs = docs[:100]
 
-    entity_corpus = get_entity_corpus("./data/entity_corpus.jsonl")
-    print(f"Entities corpus size: {len(entity_corpus)}")
+    # entity_corpus = get_entity_corpus("./data/entity_corpus.jsonl")
+    # print(f"Entities corpus size: {len(entity_corpus)}")
 
-    config = BlinkCrossEncoderConfig(
-        bert_model="./data/entity_disembiguation/blink/crossencoder",
-    )
-    model = BlinkCrossEncoder(entity_corpus, config)
+    # config = BlinkCrossEncoderConfig(
+    #     bert_model="./data/entity_disembiguation/blink/crossencoder",
+    # )
+    # model = BlinkCrossEncoder(entity_corpus, config)
 
-    tensor_data, _ = model._preprocess_docs(train_docs, is_training=True, verbose=True)
-    torch.save(tensor_data, args.output_path)
+    # tensor_data, _ = model._preprocess_docs(train_docs, is_training=True, verbose=True)
+    # torch.save(tensor_data, args.output_path)
