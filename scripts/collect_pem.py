e1b3f0b3c01bad18e7cd64f69cee29c291d36bfb
Panuthep <falcon_270394@hotmail.co.th>
2024-05-10 17:22:44 +0700

    add collect_pem.py

---
+++ scripts/collect_pem.py
@@ -0,0 +1,18 @@
+import json
+from tqdm import tqdm
+from collections import defaultdict
+
+
+if __name__ == "__main__":
+    pem = defaultdict(int)
+    with open("./data/datasets/wikipedia/training_dataset.jsonl", "r") as f:
+        for line in tqdm(f, total=6185825):
+            sample = json.loads(line)
+            text = sample["text"]
+            for span in sample["spans"]:
+                assert span["surface_form"] == text[span["start"]:span["start"] + span["length"]], f"Surface form mismatch: {span['surface_form']} != {text[span['start']:span['start'] + span['length']]}"
+                surface_form = text[span["start"]:span["start"] + span["length"]]
+                pem[surface_form] += 1
+    print(f"PEM: {len(pem)}")
+    with open("./data/pem.json", "w") as f:
+        json.dump(pem, f, indent=4)
